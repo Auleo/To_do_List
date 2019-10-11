5c018d8c884b416eabcd3a781f1b37707f684729
Auleon <wrzesniak.agnieszka@gmail.com>
2019-10-11 13:59:49 +0200

    Usuwanie i dodawanien - serwer

--- scripts/script.js
+++ scripts/script.js
@@ -3,16 +3,15 @@
 // Tutaj dodacie zmienne globalne do przechowywania elementów takich jak
 // np. lista czy input do wpisywania nowego todo
 let $list;
-let $lastId = 0 ;
+let $lastId = 0;
 let $addForm;
 let rootElement;
-let liCollection = document.querySelectorAll('.test-item ');
+let liCollection = document.querySelectorAll(".test-item ");
 
-const initialList = [
+let initialList /* = [
   'Dzisiaj robię usuwanie',
   'Nakarm psa'
-  ];
-
+  ]*/;
 /* Funkcje */
 function main() {
   // Przypisuje do zmiennych/stałych kotwice do elementów html(DOM)
@@ -26,68 +25,74 @@ function main() {
 
 function prepareDOMElements() {
   // To będzie idealne miejsce do pobrania naszych elementów z drzewa DOM i zapisanie ich w zmiennych
-  $list = document.getElementById('list');
-  $modal = document.getElementById('modal');
+  $list = document.getElementById("list");
+  $modal = document.getElementById("modal");
 
   //$buttonModal = document.querySelector('#show-modal');
   //$buttonConsole = document.querySelector('#console-modal');
-  $addForm = document.getElementById('addForm');
-  $newTodoItem = document.getElementById('newTodoItem');
+  $addForm = document.getElementById("addForm");
+  $newTodoItem = document.getElementById("newTodoItem");
 }
 
 function prepareDOMEvents() {
+  $list.addEventListener("click", listClickManager);
   // Wysyłanie (dodaweanie do listy) wpisu z formularza
-  $addForm.addEventListener('submit', addTodoElement);
-  $list.addEventListener('click', listClickManager)
+  $addForm.addEventListener("submit", addTodoElement);
+  $addForm.addEventListener("keydown", function(event) {
+    if (event.keyCode == 13) {
+      addTodoElement(event);
+    }
+  });
 }
-  
+
 function addTodoElement(event) {
   event.preventDefault();
-
   let inputValue = $newTodoItem.value.trim();
-  if (inputValue !== '') {
+  if (inputValue !== "") {
     // $modal.classList.remove ('modal-show');
-
-    let newItem = createElement(inputValue);
-    $list.appendChild(newItem);
+    console.log(inputValue);
+    createNewElementToList(inputValue);
   } else {
-    console.log('hej co robisz !!!!');
+    console.log("hej co robisz !!!!");
   }
-};
+}
 
-function createElement(title) {
+function createElement(item) {
   // Tworzyc reprezentacje DOM elementu return newElement
   // return newElement
-  if (title !== '') {
-    const newElement = document.createElement('li');
-    $lastId += 1;
-    newElement.setAttribute('id', 'item-' + $lastId);
-    
-    //     dodawanie do li id 
-    const inputField = document.createElement('input');
-    inputField.setAttribute('value', title);
-    inputField.setAttribute('name', 'item-' + $lastId);
+  if (item.title !== "") {
+    const newElement = document.createElement("li");
+    if ($lastId < item.id) {
+      $lastId = item.id;
+    } else {
+      $lastId += 1;
+    }
+    newElement.setAttribute("id", "item-" + $lastId);
 
-    const delBtn = document.createElement('button');
-    delBtn.setAttribute('data-targetId', 'item-' + $lastId);
-    delBtn.setAttribute('class', 'deleteBtn');
-    delBtn.innerText = 'Usuń';
+    //     dodawanie do li id
+    const inputField = document.createElement("input");
+    inputField.setAttribute("value", item.title);
+    inputField.setAttribute("name", "item-" + $lastId);
 
-    const editBtn = document.createElement('button');
-    editBtn.setAttribute('data-targetId', 'item-' + $lastId);
-    editBtn.setAttribute('class', 'editBtn');
-    editBtn.innerText = 'Edytuj';
+    const delBtn = document.createElement("button");
+    delBtn.setAttribute("data-targetId", "item-" + $lastId);
+    delBtn.setAttribute("class", "deleteBtn");
+    delBtn.innerText = "Usuń";
 
-    const doneBtn = document.createElement('button');
-    doneBtn.setAttribute('data-targetId', 'item-' + $lastId);
-    doneBtn.setAttribute('class', 'doneBtn');
-    doneBtn.innerText = 'Wykonane';
+    const editBtn = document.createElement("button");
+    editBtn.setAttribute("data-targetId", "item-" + $lastId);
+    editBtn.setAttribute("class", "editBtn");
+    editBtn.innerText = "Edytuj";
+
+    const doneBtn = document.createElement("button");
+    doneBtn.setAttribute("data-targetId", "item-" + $lastId);
+    doneBtn.setAttribute("class", "doneBtn");
+    doneBtn.innerText = "Wykonane";
 
     newElement.appendChild(inputField);
     newElement.appendChild(editBtn);
     newElement.appendChild(doneBtn);
     newElement.appendChild(delBtn);
-    localStorage.setItem('item-' + $lastId, title)
     return newElement;
   }
   return false;
@@ -104,53 +109,94 @@ function prepareDOMElements() {
  // To będzie idealne miejsce do pobrania naszych elementów z drzewa DOM i zapisanie ich w zmiennych
   $popupInput = document.getElementById('popupInput');
 } */
-
 function prepareInitialList() {
-  // Tutaj utworzymy sobie początkowe todosy. Mogą pochodzić np. z tablicy  */
-  console.log(initialList.length);
-
-  for (i = 0; i < initialList.length; i += 1) {
-    console.log(i);
-    console.log(initialList[i]);
-    addNewElementToList(initialList[i]);
-  };
-  /*$addTodo.addEventListener('keydown', function(event){
-    if(event.keyCode == 13){
-      addNewElementToList(addTodo);
-    } 
-  });*/
+  // Tutaj utworzymy sobie początkowe todosy. Mogą pochodzić np. z tablicy
+  pobierzListeToDo();
 }
 
 function addTodo(element, index, tablica) {
-  console.log('init: '+ element);
+  console.log("init: " + element);
   addNewElementToList(element);
 }
 
-/* Treść pola input pobrana wcześniej przekazana jako parametr */
-function addNewElementToList(title) {
-  // obsługa dodawanie elementów do listy
-  const newElement = createElement(title);
-  $list.appendChild(newElement);
+/* Treść pola input pobrana wcześniej przekazana jako parametr zaczynam używać z linijki 131 */
+function addNewElementToList(item) {
+          // obsługa dodawanie elementów do listy
+        const newElement = createElement(item);
+        $list.appendChild(newElement);
 }
+
+function createNewElementToList(item) {
+  console.log('item');
+  console.log(item);
+  if (item === null) {
+    console.log('item jest puste');
+    return false;
+  }
+  let requestData = new FormData();
+  requestData.append("title", item);
+  requestData.append("description", "Test Agnieszki");
+  requestData.append("priority", 5);
+  requestData.append("author", "Agnieszka Wrześnmiak");
+  requestData.append("extra", "active");
+  requestData.append("url", "www.ocochodzi.pl");
+  //requestData.append("parent_todo_id", 0);
+  console.log("dodaj item ");
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      let responseArray = JSON.parse(this.response);
+      if (responseArray.statys > 0) {
+        console.log(responseArray);
+      } else {
+        // obsługa dodawanie elementów do listy
+        const newElement = createElement(item);
+        $list.appendChild(newElement);
+      }
+    } else {
+      console.log("nie dodane");
+      // kaszana
+    }
+  };
+  xhr.open("POST", "http://195.181.210.249:3000/todo/", true);
+  xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+  xhr.send(requestData);
+}
+/* trzeba si zabezpieczyć,żeby nie dodawać false
+function addNewElementToList(item) {
+  // obsługa dodawanie elementów do listy
+  const newElement = createElement(item);
+  if (newElement) {
+    $list.appendChild(newElement);
+  }
+
 
 /* Działa po kliknieciu submit w formularzu dodawania nowego itema */
 function listClickManager(event) {
   // Rozstrzygnięcie co dokładnie zostało kliknięte i wywołanie odpowiedniej funkcji
-  let id = event.target.getAttribute('data-targetid');
-  console.log(event.target.className);
-
-  if (event.target.className === 'deleteBtn') {
+  let id = event.target.getAttribute("data-targetid");
+  if (event.target.className === "deleteBtn") {
     removeListElement(id);
-  } else if (event.target.className === 'editBtn') {
+  } else if (event.target.className === "editBtn") {
     editListElement(id);
-  } else if (event.target.className === 'doneBtn') {
+  } else if (event.target.className === "doneBtn") {
     markElementAsDone(id);
   }
 }
 
 function removeListElement(id) {
-  let liElement = document.querySelector('#' + id);
-  $list.removeChild(liElement);
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      let liElement = document.querySelector("#" + id);
+      $list.removeChild(liElement);
+    } else {
+      console.log("nieusuniete");
+      // kaszana
+    }
+  };
+  xhr.open("DELETE", "http://195.181.210.249:3000/todo/" + id, true);
+  xhr.send();
 }
 
 function editListElement(id, title) {
@@ -158,17 +204,14 @@ function editListElement(id, title) {
   // Umieść dane w popupie
   //openPopup();
   //$popupInput.value = title;
-  console.log('To trzeba zrobić -> editListElement');
+  console.log("To trzeba zrobić -> editListElement");
 }
 
 function markElementAsDone(id) {
   //zaznacz element jako wykonany (podmień klasę CSS)
-  let liElement = document.querySelector('#' + id);
-  liElement.className += ' done ';
-}
-
-/*
-function addDataToPopup(/* Title, author, id */ /*) {
+  let liElement = document.querySelector("#" + id);
+  liElement.className += " done ";
+} /*) {
   // umieść informacje w odpowiednim miejscu w popupie
 }
 /*
@@ -178,9 +221,91 @@ function acceptChangeHandler() {
   // closePopup()
 }
 
+
+
+
+
 function closePopup() {
   // Zamknij popup
 }
+*/ /*
+async function getListFromServer() {
+  $list.innerHTML = '';
+  try {
+      let response = await axios.get('http://195.181.210.249:3000/todo/');
+      if (response.status === 200) {
+          response.data.forEach(todo => {
+              addNewElementToList(todo.title, todo.id);
+          });
+      }
+  }
+  catch (e) {
+      console.log(e);
+  }
+}
+*/
+
+/*
+function addDataToPopup(/* Title, author, id */ function pobierzListeToDo() {
+  console.log("pobierzListeToDo");
+  const xhr = new XMLHttpRequest();
+  xhr.onreadystatechange = function() {
+    if (this.readyState === 4 && this.status === 200) {
+      console.log("todo get ok");
+      przygotujListe(this);
+    } else {
+      console.log("todo get error");
+      // kaszana
+    }
+  };
+  xhr.open("GET", "http://195.181.210.249:3000/todo/", true);
+  xhr.send();
+}
+
+function przygotujListe(odpowiedzSerwera) {
+  const listaToDo = JSON.parse(odpowiedzSerwera.response);
+  /*[].forEach.call(books, function(book) {
+        const title = book.querySelector('title').firstChild.nodeValue;
+        const author = book.querySelector('author').firstChild.nodeValue;
+        const genre = book.querySelector('genre').firstChild.nodeValue;
+        const price = Number(book.querySelector('price').firstChild.nodeValue);
+        const description = book.querySelector('description').firstChild.nodeValue;
+        console.log(title, author, gender, price, description);
+    });*/
+  initialList = listaToDo;
+  for (i = 0; i < initialList.length; i += 1) {
+    addNewElementToList(initialList[i]);
+  }
+}
+/*
+
+curl --location --request PUT "http://195.181.210.249:3000/todo/85" \
+  --header "Content-Type: application/x-www-form-urlencoded" \
+  --data "title=NowyZmieniony"
+
+let xhr = XMLHttpReguest();
+xhr.open("GET", curl --location --request GET "http://195.181.210.249:3000/todo/")
+xhr.addEventListener('load, function () {
+if(this.status === 200) {
+
+let res= JSON.parse(this.responseText);
+if (typeof callbac !== 'undefined'){
+  callback(); /czy użytkownik wyżuci coś?
+  }
+}
+});
+  / albo   console.log(this.responseText);
+}
+}
+});
+xhr.addEventlistener('errore', function () {})
+  console.log('Wystąpił błąd');
+});
+xhr.send;
+
+let promise =new Promise((resolve, reject) => { 
+    resolve ("liElement");
+} );
 
 function declineChanges() { //niepotrzebna raczej
   // closePopup()
@@ -205,4 +330,4 @@ function declineChanges() { //niepotrzebna raczej
 /**************************************************************/
 /* INIT */
 
-document.addEventListener('DOMContentLoaded', main);
+document.addEventListener("DOMContentLoaded", main);
